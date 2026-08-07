859872dd3ffa3229871012d7986bb82ca433904a
AhmedUnavailable <80337325+AhmedUnavailable@users.noreply.github.com>
2026-08-07 11:00:49 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        
+        def bt(string, op, cl):
+            if op > n or cl > op:
+                return 
+            if len(string) == 2*n:
+                res.append(string)
+                return
+
+            string = string + "("
+            bt(string, op + 1, cl)
+            
+            string = string[:-1]
+            
+            string = string + ")"
+            bt(string, op, cl + 1)
+        
+        bt("", 0, 0)
+
+        return res
